90ea1b4e87f882f89d84fcb3c3638e4701dfec74
Colin Wilson <colin.wilson@protonmail.ch>
2021-12-26 21:17:24 +0000

    ⚡ update default chart version to 10.9.1

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "namespace" {
 variable "traefik_chart_version" {
   description = "Version of Traefik chart to install"
   type        = string
-  default     = "10.7.1" # See https://artifacthub.io/packages/helm/traefik/traefik for latest version(s)
+  default     = "10.9.1" # See https://artifacthub.io/packages/helm/traefik/traefik for latest version(s)
 }
 
 # Helm chart deployment can sometimes take longer than the default 5 minutes
